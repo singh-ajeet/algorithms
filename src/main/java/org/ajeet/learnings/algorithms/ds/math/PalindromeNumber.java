772df9b8f81cce39e0e341815da318cfd14fac66
Ajeet Singh <ajeetsinghtomer@gmail.com>
2019-09-21 22:05:06 +0530

    Knapsack problem

---
+++ src/main/java/org/ajeet/learnings/algorithms/ds/math/PalindromeNumber.java
@@ -0,0 +1,24 @@
+package org.ajeet.learnings.algorithms.ds.math;
+
+public final class PalindromeNumber {
+
+    public static int check(int number) {
+        int div = 1;
+        while (number/div >= 10)
+            div = div * 10;
+
+        while(number != 0) {
+            int first = number/div;
+            System.out.println(first);
+            number = number % div; // Remove first digit
+            number = number/10;  // Remove last digit
+            div = div/100; // We reduced two digits so div need to shorten by two digits
+        }
+
+        return div;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check(12321));
+    }
+}
